d3387e5425b93f9f662ab5313e406b07a8d718f1
Kendall McGree <mcgreekendall@gmail.com>
2022-05-16 16:47:38 -0700

    add tests for user'sm properties, all pass

--- test/User-test.js
+++ test/User-test.js
@@ -38,4 +38,43 @@ describe("User", () => {
   it("Should be an instance of User", () => {
     expect(user1).to.be.an.instanceOf(User);
   });
+
+  it("Should take in user's id", () => {
+    expect(user1.id).to.equal(1);
+  });
+
+  it("Should take in user's name", () => {
+    expect(user1.name).to.equal("Luisa Hane");
+  });
+
+  it("Should take in user's address", () => {
+    expect(user1.address).to.equal(
+      "15195 Nakia Tunnel, Erdmanport VA 19901-1697"
+    );
+  });
+
+  it("Should take in user's email", () => {
+    expect(user1.email).to.equal("Diana.Hayes1@hotmail.com");
+  });
+
+  it("Should take in user's stride length", () => {
+    expect(user1.strideLength).to.equal(4.3);
+  });
+
+  it("Should take in user's daily step goals", () => {
+    expect(user1.dailyStepGoal).to.equal(10000);
+  });
+
+  it("Should take in user's friends", () => {
+    expect(user1.friends).to.deep.equal([16, 4, 8]);
+  });
 });
+
+// id: 1,
+// name: "Luisa Hane",
+// address: "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+// email: "Diana.Hayes1@hotmail.com",
+// strideLength: 4.3,
+// dailyStepGoal: 10000,
+// friends: [16, 4, 8],
+// };
