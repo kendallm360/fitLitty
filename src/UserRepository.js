04f05452054707009c4205a797b665236eaf3276
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-05-18 16:38:19 -0700

    Build  method to display weekly fluid intake

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -17,17 +17,43 @@ class UserRepository {
     return parseInt(average.toFixed());
   }
 
+  filterById(id) {
+    const filtered = this.hydrationData.filter(user => {
+      return id === user.userID;
+     })
+     return filtered;
+  }
+
   getAverageFluidIntake(id) {
-    const filteredHydration = this.hydrationData.filter(user => {
-     return id === user.userID;
-    })
+    const filteredHydration = this.filterById(id);
     const totalHydration = filteredHydration.reduce((sum, user) => {
       sum += user.numOunces;
       return sum;
     }, 0)
     const avg = totalHydration / filteredHydration.length;
-      return parseInt(avg.toFixed())
+    return parseInt(avg.toFixed())
   }
+
+   getFluidIntakeByDate(id, date){
+     const filteredFluidById = this.filterById(id);
+     const fluidByDate = filteredFluidById.find(data => {
+      return data.date === date;
+     })
+     return fluidByDate.numOunces;
+   }
+
+  getDailyFluidIntakeByWeek(id, dateSelected){
+    const filteredFluidById = this.filterById(id);
+    const index = filteredFluidById.findIndex(data => {
+      return data.date === dateSelected
+    })
+    
+    const week = filteredFluidById.slice((index - 6) , (index + 1))
+      .map(data => {
+        return data.numOunces
+      })
+    return week;
+   }
 }
 
 export default UserRepository;
