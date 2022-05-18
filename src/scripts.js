541971d96c77ad8218a152d5d62463543b0b8936
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-05-17 17:26:26 -0700

    Diplay user info in contact card and welcome message

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,7 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
-console.log(userData,"<>>>>userData")
+// console.log(userData,"<>>>>userData")
 // An example of how you tell webpack to use a CSS file
 import './css/styles.css';
 
@@ -15,3 +15,52 @@ console.log('This is the JavaScript entry file - your code begins here.');
 import userData from './data/users';
 
 import UserRepository from './UserRepository';
+import User from './User';
+
+//imports
+
+//Query selectors
+// Create query selectors to target areas we want info displayed
+const contactCard = document.querySelector(".user-info");
+const welcomeUser = document.querySelector(".welcome");
+const activityWidget = document.querySelector(".steps");
+
+//global variables
+let userRepo;
+let currentUser;
+let userDataInstances;
+//functions
+// Iterate through user data and create instances of users stored in array in variable(in script.js)
+// Create a new UserRepository instance and pass it the users array
+
+userDataInstances = userData.map(function (data) {
+        return new User(data)});
+
+function createUserRepo() {
+  userRepo = new UserRepository(userDataInstances)
+  currentUser = userRepo.findById(10);
+};
+
+function displayUserDetails(){
+  contactCard.innerText = `
+  Name: ${currentUser.name}
+  Address: ${currentUser.address}
+  Email: ${currentUser.email}`;
+}
+
+function greetUser () {
+  welcomeUser.innerText = `Welcome, ${currentUser.returnFirstName()}!`
+}
+
+
+//  function to load info to contact card
+//  Function that interpolates users stepgoal compared to other users
+//"Heres your step goal compared to others: Yours:   Other users: "
+
+//eventlistener
+window.addEventListener('load', () => {
+createUserRepo();
+displayUserDetails();
+greetUser();
+
+});
