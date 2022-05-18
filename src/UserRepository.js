89e061ac94363b8ea7b650410484a30864f3ca2c
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-05-18 14:18:03 -0700

    Build and test getAverageFluidIntake method

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -9,12 +9,24 @@ class UserRepository {
   }
 
   getAverageStepGoal() {
-    let average = this.users.reduce((sum, user) => {
+    let totalSteps = this.users.reduce((sum, user) => {
       sum += user.dailyStepGoal;
       return sum;
     }, 0);
-    let test = average / this.users.length;
-    return parseInt(test.toFixed());
+    let average = totalSteps / this.users.length;
+    return parseInt(average.toFixed());
+  }
+
+  getAverageFluidIntake(id) {
+    const filteredHydration = this.hydrationData.filter(user => {
+     return id === user.userID;
+    })
+    const totalHydration = filteredHydration.reduce((sum, user) => {
+      sum += user.numOunces;
+      return sum;
+    }, 0)
+    const avg = totalHydration / filteredHydration.length;
+      return parseInt(avg.toFixed())
   }
 }
 
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,7 +1,8 @@
 import { expect } from "chai";
+import Hydration from "../src/Hydration";
 import UserRepository from "../src/UserRepository";
 
-describe("User Repository", () => {
+describe("User Data", () => {
   let userRepo;
   let testUsers;
 
@@ -58,3 +59,55 @@ describe("User Repository", () => {
     expect(userRepo.getAverageStepGoal()).to.equal(6667);
   });
 });
+
+describe("Hydration Data", () => {
+  let hydrationData;
+  let userRepo;
+  let testUsers; 
+
+  beforeEach(() => {
+    testUsers = [
+      {
+        id: 1,
+        name: "Luisa Hane",
+        address: "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+        email: "Diana.Hayes1@hotmail.com",
+        strideLength: 4.3,
+        dailyStepGoal: 10000,
+        friends: [16, 4, 8],
+      },
+      {
+        id: 2,
+        name: "Jarvis Considine",
+        address: "30086 Kathryn Port, Ciceroland NE 07273",
+        email: "Dimitri.Bechtelar11@gmail.com",
+        strideLength: 4.5,
+        dailyStepGoal: 5000,
+        friends: [9, 18, 24, 19],
+      },
+      {
+        id: 3,
+        name: "Herminia Witting",
+        address: "85823 Bosco Fork, East Oscarstad MI 85126-5660",
+        email: "Elwin.Tromp@yahoo.com",
+        strideLength: 4.4,
+        dailyStepGoal: 5000,
+        friends: [19, 11, 42, 33],
+      },
+    ]
+    hydrationData = [
+      {"userID":1,"date":"2019/06/15","numOunces":37},
+      {"userID":2,"date":"2019/06/15","numOunces":75},
+      {"userID":1,"date":"2019/06/16","numOunces":69},
+      {"userID":1,"date":"2019/06/17","numOunces":96}
+    ]
+
+    userRepo = new UserRepository(testUsers, hydrationData)
+
+    
+  })
+
+  it("should average a users fluid intake", () => {
+    expect(userRepo.getAverageFluidIntake(1)).to.equal(67)
+  })
+})
