6a62c09da22339d4cb7c2919ab8c67772e03039d
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-05-17 20:51:48 -0700

    Create fetch calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,28 @@
 // Your fetch requests will live here!
 
+function getUserData() {
+  return fetch('https://fitlit-api.herokuapp.com/api/v1/users')
+  .then(response => response.json())
+};
 
-console.log('I will be a fetch request!')
+function getSleepData() {
+  return fetch('https://fitlit-api.herokuapp.com/api/v1/sleep')
+  .then(response => response.json())
+};
 
+function getActivityData() {
+  return fetch('https://fitlit-api.herokuapp.com/api/v1/activity')
+  .then(response => response.json())
+};
+
+function getHydrationData() {
+  return fetch('https://fitlit-api.herokuapp.com/api/v1/hydration')
+  .then(response => response.json())
+};
+
+export {
+  getSleepData,
+  getActivityData,
+  getUserData,
+  getHydrationData
+}
--- src/scripts.js
+++ src/scripts.js
@@ -1,26 +1,17 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
+import {
+  getSleepData,
+  getActivityData,
+  getUserData,
+  getHydrationData
+} from './apiCalls.js'
 
-// console.log(userData,"<>>>>userData")
-// An example of how you tell webpack to use a CSS file
 import './css/styles.css';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
-console.log('This is the JavaScript entry file - your code begins here.');
-
-// An example of how you tell webpack to use a JS file
-
-import userData from './data/users';
 
 import UserRepository from './UserRepository';
 import User from './User';
 
-//imports
-
 //Query selectors
-// Create query selectors to target areas we want info displayed
 const contactCard = document.querySelector(".user-info");
 const welcomeUser = document.querySelector(".welcome");
 const activityWidget = document.querySelector(".steps");
@@ -28,17 +19,34 @@ const activityWidget = document.querySelector(".steps");
 //global variables
 let userRepo;
 let currentUser;
-let userDataInstances;
-//functions
-// Iterate through user data and create instances of users stored in array in variable(in script.js)
-// Create a new UserRepository instance and pass it the users array
+let userData;
+let sleepData;
+let hydrationData;
+let activityData;
 
-userDataInstances = userData.map(function (data) {
-        return new User(data)});
+//functions
+function userDataInstances() {
+  return userData.map(function (data) {
+    return new User(data)
+  });
+}
+
+function fetchUsers() {
+  Promise.all([getUserData(),getSleepData(), getHydrationData(), getActivityData()]).then((data) => {
+    userData = data[0].userData;
+    sleepData = data[1].sleepData;
+    hydrationData = data[2].hydrationData;
+    activityData = data[3].activityData;
+    createUserRepo();
+    displayUserDetails();
+    greetUser();
+    compareSteps();
+  })
+}
 
 function createUserRepo() {
-  userRepo = new UserRepository(userDataInstances)
-  currentUser = userRepo.findById(10);
+  userRepo = new UserRepository(userDataInstances())
+  currentUser = userRepo.findById(11);
 };
 
 function displayUserDetails(){
@@ -53,18 +61,12 @@ function greetUser () {
 }
 
 function compareSteps() {
-  activityWidget.innerText = `Hey ${currentUser.name}! This is how your step goal compares to other users!
+  activityWidget.innerText = `Hey ${currentUser.returnFirstName()}!
+  This is how your step goal compares to other users!
   Yours: ${currentUser.dailyStepGoal} vs Theirs: ${userRepo.getAverageStepGoal()}`
 }
-//  function to load info to contact card
-//  Function that interpolates users stepgoal compared to other users
-//"Heres your step goal compared to others: Yours:   Other users: "
 
 //eventlistener
 window.addEventListener('load', () => {
-createUserRepo();
-displayUserDetails();
-greetUser();
-compareSteps();
-
+fetchUsers();
 });
