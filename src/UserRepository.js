81d8d32688dc724097634f2838c83fd277255a95
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-05-18 21:43:02 -0700

    Refactor UserRepository methods to be dynamic

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -4,57 +4,69 @@ class UserRepository {
     this.hydrationData = hydrationData;
   }
 
+//helper methods
+//this.filterById(id, "hydrationData");
+  filterById(id, attr) {
+    // attr = "hydrationData" || "sleepData" || "activityData"
+    const filtered = this[attr].filter(obj => {
+      return id === obj.userID;
+    })
+    return filtered;
+  }
+//this.getAverage(filteredHydration, "numOunces")
+  getAverage(arr, attr) {
+    let total = arr.reduce((sum, obj) => {
+      sum += obj[attr];
+      return sum;
+    }, 0);
+    let average = total / arr.length;
+    return parseInt(average.toFixed());
+  }
+//this.getDataByDate(filteredFluidById, date, "numOunces")
+  getDataByDate(arr, date, attr) {
+    const objByDate = arr.find(data => {
+      return data.date === date;
+    })
+    return objByDate[attr];
+  }
+//this.getDataByWeek(filteredFluidById, dateSelected, "fluidOz", "numOunces")
+  getDataByWeek(arr, dateSelected, key, attr){
+    const index = arr.findIndex(data => {
+      return data.date === dateSelected
+    })
+    const week = arr.slice((index - 6) , (index + 1))
+      .map(data => {
+        return {date: data.date, [key]: data[attr]}
+      })
+    return week;
+  }
+
+//User methods
   findById(id) {
     return this.users.find((user) => user.id === id);
   }
 
   getAverageStepGoal() {
-    let totalSteps = this.users.reduce((sum, user) => {
-      sum += user.dailyStepGoal;
-      return sum;
-    }, 0);
-    let average = totalSteps / this.users.length;
-    return parseInt(average.toFixed());
+    return this.getAverage(this.users, "dailyStepGoal")
   }
 
-  filterById(id) {
-    const filtered = this.hydrationData.filter(user => {
-      return id === user.userID;
-     })
-     return filtered;
-  }
-
+//hydration methods
   getAverageFluidIntake(id) {
-    const filteredHydration = this.filterById(id);
-    const totalHydration = filteredHydration.reduce((sum, user) => {
-      sum += user.numOunces;
-      return sum;
-    }, 0)
-    const avg = totalHydration / filteredHydration.length;
-    return parseInt(avg.toFixed())
+    const filteredHydration = this.filterById(id, "hydrationData");
+    return this.getAverage(filteredHydration, "numOunces")
   }
 
    getFluidIntakeByDate(id, date){
-     const filteredFluidById = this.filterById(id);
-     const fluidByDate = filteredFluidById.find(data => {
-      return data.date === date;
-     })
-     return fluidByDate.numOunces;
+     const filteredFluidById = this.filterById(id, "hydrationData");
+     return this.getDataByDate(filteredFluidById, date, "numOunces")
    }
 
   getDailyFluidIntakeByWeek(id, dateSelected){
-    const filteredFluidById = this.filterById(id);
-    const index = filteredFluidById.findIndex(data => {
-      return data.date === dateSelected
-    })
-    const week = filteredFluidById.slice((index - 6) , (index + 1))
-      .map(data => {
-        return {date: data.date, fluidOz: data.numOunces}
-      })
-    return week;
-   }
+    const filteredFluidById = this.filterById(id, "hydrationData");
+    return this.getDataByWeek(filteredFluidById, dateSelected, "fluidOz", "numOunces")
+  }
 
-   
+//sleep methods
 }
 
 export default UserRepository;
