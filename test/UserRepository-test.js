c8b70a9e1b0d0ef59c46e4dde357bed9331db730
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-05-19 14:23:09 -0700

    Create averageSleep method and test

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -141,4 +141,76 @@ describe("Hydration Data", () => {
     {date:"2019/06/20", fluidOz:69},
     {date:"2019/06/21", fluidOz:96}])
   })
-})
+});
+
+describe("Sleep Data", () => {
+  let hydrationData;
+  let userRepo;
+  let testUsers;
+  let sleepData;
+
+  beforeEach(() => {
+    testUsers = [
+      {
+        id: 1,
+        name: "Luisa Hane",
+        address: "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+        email: "Diana.Hayes1@hotmail.com",
+        strideLength: 4.3,
+        dailyStepGoal: 10000,
+        friends: [16, 4, 8],
+      },
+      {
+        id: 2,
+        name: "Jarvis Considine",
+        address: "30086 Kathryn Port, Ciceroland NE 07273",
+        email: "Dimitri.Bechtelar11@gmail.com",
+        strideLength: 4.5,
+        dailyStepGoal: 5000,
+        friends: [9, 18, 24, 19],
+      },
+      {
+        id: 3,
+        name: "Herminia Witting",
+        address: "85823 Bosco Fork, East Oscarstad MI 85126-5660",
+        email: "Elwin.Tromp@yahoo.com",
+        strideLength: 4.4,
+        dailyStepGoal: 5000,
+        friends: [19, 11, 42, 33],
+      },
+    ]
+    hydrationData = [
+      {"userID":1,"date":"2019/06/14","numOunces":96},
+      {"userID":1,"date":"2019/06/15","numOunces":37},
+      {"userID":2,"date":"2019/06/15","numOunces":75},
+      {"userID":1,"date":"2019/06/16","numOunces":69},
+      {"userID":1,"date":"2019/06/17","numOunces":96},
+      {"userID":1,"date":"2019/06/18","numOunces":69},
+      {"userID":1,"date":"2019/06/19","numOunces":96},
+      {"userID":1,"date":"2019/06/20","numOunces":69},
+      {"userID":1,"date":"2019/06/21","numOunces":96},
+      {"userID":1,"date":"2019/06/22","numOunces":96}
+    ];
+
+    sleepData = [
+      {"userID":1,"date":"2019/06/15","hoursSlept":6.1,"sleepQuality":2.2},
+      {"userID":1,"date":"2019/06/16","hoursSlept":6.6,"sleepQuality":2},
+      {"userID":1,"date":"2019/06/17","hoursSlept":8.2,"sleepQuality":3},
+      {"userID":1,"date":"2019/06/18","hoursSlept":5.0,"sleepQuality":2.6},
+      {"userID":1,"date":"2019/06/19","hoursSlept":6.5,"sleepQuality":2.3},
+      {"userID":2,"date":"2019/06/19","hoursSlept":6.1,"sleepQuality":2.4},
+      {"userID":1,"date":"2019/06/20","hoursSlept":7.0,"sleepQuality":4},
+      {"userID":1,"date":"2019/06/21","hoursSlept":8.5,"sleepQuality":1.2},
+      {"userID":1,"date":"2019/06/22","hoursSlept":6.5,"sleepQuality":3.1},
+      {"userID":1,"date":"2019/06/23","hoursSlept":6.7,"sleepQuality":4.4},
+      {"userID":1,"date":"2019/06/24","hoursSlept":7.5,"sleepQuality":4.8}
+    ]
+
+    userRepo = new UserRepository(testUsers, hydrationData, sleepData)
+
+    })
+
+    it("should show average of hours slept per day for a user", () => {
+      expect(userRepo.getAverageSleep(1)).to.equal(7)
+    })
+  });
