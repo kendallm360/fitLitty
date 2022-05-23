e8a5ecd676f7ec4ca24704045ce445cad5539914
Kendall McGree <mcgreekendall@gmail.com>
2022-05-23 13:31:36 -0700

    minor updates to code format

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,12 +1,7 @@
-// Your fetch requests will live here!
-
-function getData(apiName) {
-  return fetch(`https://fitlit-api.herokuapp.com/api/v1/${apiName}`)
-  .then(response => response.json())
+const getData = (apiName) => {
+  return fetch(`https://fitlit-api.herokuapp.com/api/v1/${apiName}`).then(
+    (response) => response.json()
+  );
 };
 
-
-
-export {
-  getData
-}
+export { getData };
