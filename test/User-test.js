58c7ed3dc6bce75f43cb9049450c6cd21ba638ce
Kendall McGree <mcgreekendall@gmail.com>
2022-06-01 11:27:20 -0500

    make site generate random user and add sad paths to tests

--- test/User-test.js
+++ test/User-test.js
@@ -4,8 +4,10 @@ import User from "../src/User";
 describe("User", () => {
   let user1;
   let user2;
+  let user3;
   let userData1;
   let userData2;
+  let userData3;
 
   beforeEach(() => {
     userData1 = {
@@ -27,8 +29,18 @@ describe("User", () => {
       dailyStepGoal: 5000,
       friends: [9, 18, 24, 19],
     };
+    userData3 = {
+      id: undefined,
+      name: undefined,
+      address: undefined,
+      email: undefined,
+      strideLength: undefined,
+      dailyStepGoal: undefined,
+      friends: undefined,
+    };
     user1 = new User(userData1);
     user2 = new User(userData2);
+    user3 = new User(userData3);
   });
 
   it("Should be a function", () => {
@@ -42,6 +54,10 @@ describe("User", () => {
   it("Should take in user's id", () => {
     expect(user1.id).to.equal(1);
   });
+
+  // it("Should throw an error if no id is available", () => {
+  //   expect(user3.id).to.equal(?);
+  // });
 
   it("Should take in user's name", () => {
     expect(user1.name).to.equal("Luisa Hane");
@@ -71,5 +87,11 @@ describe("User", () => {
 
   it("Should have a method that returns the user's first name", () => {
     expect(user1.returnFirstName()).to.equal("Luisa");
+  });
+
+  it("The method should return a message if a name is missing", () => {
+    expect(user3.returnFirstName()).to.equal(
+      "Oops it looks like your name is missing from our data base"
+    );
   });
 });
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,41 +1,41 @@
 import { expect } from "chai";
 import UserRepository from "../src/UserRepository";
-
+import { userRepoInstance } from "./mockData/mock";
 describe("User Data", () => {
   let userRepo;
-  let testUsers;
+  // let testUsers;
 
   beforeEach(() => {
-    testUsers = [
-      {
-        id: 1,
-        name: "Luisa Hane",
-        address: "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-        email: "Diana.Hayes1@hotmail.com",
-        strideLength: 4.3,
-        dailyStepGoal: 10000,
-        friends: [16, 4, 8],
-      },
-      {
-        id: 2,
-        name: "Jarvis Considine",
-        address: "30086 Kathryn Port, Ciceroland NE 07273",
-        email: "Dimitri.Bechtelar11@gmail.com",
-        strideLength: 4.5,
-        dailyStepGoal: 5000,
-        friends: [9, 18, 24, 19],
-      },
-      {
-        id: 3,
-        name: "Herminia Witting",
-        address: "85823 Bosco Fork, East Oscarstad MI 85126-5660",
-        email: "Elwin.Tromp@yahoo.com",
-        strideLength: 4.4,
-        dailyStepGoal: 5000,
-        friends: [19, 11, 42, 33],
-      },
-    ];
-    userRepo = new UserRepository(testUsers);
+    // testUsers = [
+    //   {
+    //     id: 1,
+    //     name: "Luisa Hane",
+    //     address: "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+    //     email: "Diana.Hayes1@hotmail.com",
+    //     strideLength: 4.3,
+    //     dailyStepGoal: 10000,
+    //     friends: [16, 4, 8],
+    //   },
+    //   {
+    //     id: 2,
+    //     name: "Jarvis Considine",
+    //     address: "30086 Kathryn Port, Ciceroland NE 07273",
+    //     email: "Dimitri.Bechtelar11@gmail.com",
+    //     strideLength: 4.5,
+    //     dailyStepGoal: 5000,
+    //     friends: [9, 18, 24, 19],
+    //   },
+    //   {
+    //     id: 3,
+    //     name: "Herminia Witting",
+    //     address: "85823 Bosco Fork, East Oscarstad MI 85126-5660",
+    //     email: "Elwin.Tromp@yahoo.com",
+    //     strideLength: 4.4,
+    //     dailyStepGoal: 5000,
+    //     friends: [19, 11, 42, 33],
+    //   },
+    // ];
+    userRepo = userRepoInstance;
   });
 
   it("should be a function", function () {
@@ -46,15 +46,27 @@ describe("User Data", () => {
     expect(userRepo).to.be.an.instanceOf(UserRepository);
   });
 
-  it("Should take in users", () => {
-    expect(userRepo.users).to.equal(testUsers);
+  it("Should be able to take in multiple users", () => {
+    expect(userRepo.users).to.be.a("array");
+    expect(userRepo.users[0]).to.be.a("object");
+    expect(userRepo.users).to.equal(userRepoInstance.users);
   });
 
   it("Should have a method that finds users by id", () => {
-    expect(userRepo.findById(3)).to.equal(testUsers[2]);
+    expect(userRepo.findById(3)).to.equal(userRepoInstance.users[2]);
+  });
+
+  it("The method should return a message if id doesn't exist", () => {
+    expect(userRepo.findById()).to.equal(
+      "Oops it looks like no id was passed in"
+    );
   });
 
   it("Should have a method that shows average step goals", () => {
     expect(userRepo.getAverageStepGoal()).to.equal(6667);
+  });
+
+  it("Should have a method that generates a random user", () => {
+    expect(userRepo.generateRandomUser()).to.be.a("object");
   });
 });
