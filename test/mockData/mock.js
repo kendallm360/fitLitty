4d9c8196a5a30972e89a4ad9be438db1bf70565a
Kendall McGree <mcgreekendall@gmail.com>
2022-06-01 11:47:54 -0500

    add util-test variables to mock data file

--- test/mockData/mock.js
+++ test/mockData/mock.js
@@ -30,4 +30,53 @@ const userRepoInstance = new UserRepository([
   },
 ]);
 
-export { userRepoInstance };
+const userHydrationData = [
+  { userID: 1, date: "2019/06/14", numOunces: 96 },
+  { userID: 1, date: "2019/06/15", numOunces: 37 },
+  { userID: 1, date: "2019/06/16", numOunces: 69 },
+  { userID: 1, date: "2019/06/17", numOunces: 96 },
+  { userID: 1, date: "2019/06/18", numOunces: 69 },
+  { userID: 1, date: "2019/06/19", numOunces: 96 },
+  { userID: 1, date: "2019/06/20", numOunces: 69 },
+  { userID: 1, date: "2019/06/21", numOunces: 96 },
+  { userID: 1, date: "2019/06/22", numOunces: 96 },
+];
+
+const userSleepData = [
+  { userID: 1, date: "2019/06/15", hoursSlept: 6.1, sleepQuality: 2.2 },
+  { userID: 1, date: "2019/06/16", hoursSlept: 6.6, sleepQuality: 2 },
+  { userID: 1, date: "2019/06/17", hoursSlept: 8.2, sleepQuality: 3 },
+  { userID: 1, date: "2019/06/18", hoursSlept: 5.0, sleepQuality: 2.7 },
+  { userID: 1, date: "2019/06/19", hoursSlept: 6.5, sleepQuality: 2.3 },
+  { userID: 1, date: "2019/06/20", hoursSlept: 7.0, sleepQuality: 4 },
+  { userID: 1, date: "2019/06/21", hoursSlept: 8.5, sleepQuality: 1.2 },
+  { userID: 1, date: "2019/06/22", hoursSlept: 6.5, sleepQuality: 3.1 },
+  { userID: 1, date: "2019/06/23", hoursSlept: 6.7, sleepQuality: 4.4 },
+  { userID: 1, date: "2019/06/24", hoursSlept: 7.5, sleepQuality: 4.8 },
+];
+
+const weeklyHoursSlept = [
+  { date: "2019/06/15", hoursSlept: 6 },
+  { date: "2019/06/16", hoursSlept: 7 },
+  { date: "2019/06/17", hoursSlept: 8 },
+  { date: "2019/06/18", hoursSlept: 5 },
+  { date: "2019/06/19", hoursSlept: 7 },
+  { date: "2019/06/20", hoursSlept: 7 },
+  { date: "2019/06/21", hoursSlept: 9 },
+];
+const weeklyWater = [
+  { date: "2019/06/15", numOunces: 37 },
+  { date: "2019/06/16", numOunces: 69 },
+  { date: "2019/06/17", numOunces: 96 },
+  { date: "2019/06/18", numOunces: 69 },
+  { date: "2019/06/19", numOunces: 96 },
+  { date: "2019/06/20", numOunces: 69 },
+  { date: "2019/06/21", numOunces: 96 },
+];
+export {
+  userRepoInstance,
+  userHydrationData,
+  userSleepData,
+  weeklyHoursSlept,
+  weeklyWater,
+};
